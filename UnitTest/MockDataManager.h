2c7c4a0f60255f23d90d9a0ef1458fe6eadc3de3
Unsang Kwon <Unsang.Kwon@gmail.com>
2022-03-04 16:35:35 +0900

    test: delete Processor test code (mock fake)
    - mock fake를 활용하여 delete command의 delete type 별 test code 작성

---
+++ UnitTest/MockDataManager.h
@@ -0,0 +1,247 @@
+#pragma once
+
+#include "pch.h"
+#include <stdexcept>
+#include "../AceAgent/Processor.h"
+#include "../AceAgent/SelectType.h"
+#include "../AceAgent/ProcessResult.h"
+#include <map>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+class FakeDataManager : public IDataManager {
+public:
+	FakeDataManager() {
+	}
+	virtual void addEmployee(EmployeeInfomation& employeeInfomation)
+	{
+		unsigned int employeeNumber = employeeInfomation.getEmployeeNumber();
+		string fullName = employeeInfomation.getFirstName() +" " + employeeInfomation.getLastName();
+		unsigned int fullPhone = (employeeInfomation.getMidPhoneNumber() * 10000) + employeeInfomation.getLastPhoneNumber();
+		unsigned int fullBrithday = (employeeInfomation.getBirthday().getYear() * 10000) + (employeeInfomation.getBirthday().getMonth() * 100)+ (employeeInfomation.getBirthday().getYear() * 1);
+
+		fakeDataManager_.insert(make_pair(employeeNumber, employeeInfomation));
+		fakeDataManagerFullName_[fullName].push_back(employeeNumber);
+		fakeDataManagerFristName_[employeeInfomation.getFirstName()].push_back(employeeNumber);
+		fakeDataManagerLastName_[employeeInfomation.getLastName()].push_back(employeeNumber);
+		fakeDataManagerCareerLevel_[employeeInfomation.getCareerLevel()].push_back(employeeNumber);;
+		fakeDataManagerFullPhoneNumber_[fullPhone].push_back(employeeNumber);;
+		fakeDataManagerMidPhoneNumber_[employeeInfomation.getMidPhoneNumber()].push_back(employeeNumber);;
+		fakeDataManagerLastPhoneNumber_[employeeInfomation.getLastPhoneNumber()].push_back(employeeNumber);;
+		fakeDataManagerFullBirthDay_[fullBrithday].push_back(employeeNumber);;
+		fakeDataManagerYearOfBirthday_[employeeInfomation.getBirthday().getYear()].push_back(employeeNumber);;
+		fakeDataManagerMonthOfBirthday_[employeeInfomation.getBirthday().getMonth()].push_back(employeeNumber);;
+		fakeDataManagerDayOfBirthday_[employeeInfomation.getBirthday().getDay()].push_back(employeeNumber);;
+		fakeDataManagerCertiLevel_[employeeInfomation.getCertiLevel()].push_back(employeeNumber);;
+
+	}
+
+	virtual ProcessResult* deleteEmployee(const bool isDetailPrint, const SelectType deleteType, EmployeeInfomation& deleteInfomation)
+	{		
+		unsigned int employeeNumber = deleteInfomation.getEmployeeNumber();
+		string fullName = deleteInfomation.getFirstName() + " " + deleteInfomation.getLastName();
+		unsigned int fullPhone = (deleteInfomation.getMidPhoneNumber() * 10000) + deleteInfomation.getLastPhoneNumber();
+		unsigned int fullBrithday = (deleteInfomation.getBirthday().getYear() * 10000) + (deleteInfomation.getBirthday().getMonth() * 100) + (deleteInfomation.getBirthday().getYear() * 1);
+
+		if (SelectType::EMPLOYEE_NUMBER == deleteType)
+			return deleteEmployeeByEmployeeNumber(deleteInfomation.getEmployeeNumber());
+		else if (SelectType::FULL_NAME == deleteType)
+			return deleteEmployeeMap(fakeDataManagerFullName_, fullName);
+		else if (SelectType::FIRST_NAME == deleteType)
+			return deleteEmployeeMap(fakeDataManagerFristName_, deleteInfomation.getFirstName());
+		else if (SelectType::LAST_NAME == deleteType)
+			return deleteEmployeeMap(fakeDataManagerLastName_, deleteInfomation.getLastName());
+		else if (SelectType::CAREER_LEVEL == deleteType)
+			return deleteEmployeeMap(fakeDataManagerCareerLevel_, deleteInfomation.getCareerLevel());
+		else if (SelectType::FULL_PHONE_NUMBER == deleteType)
+			return deleteEmployeeMap(fakeDataManagerFullPhoneNumber_, fullPhone);
+		else if (SelectType::MID_PHONE_NUMBER == deleteType)
+			return deleteEmployeeMap(fakeDataManagerMidPhoneNumber_, deleteInfomation.getMidPhoneNumber());
+		else if (SelectType::LAST_PHONE_NUMBER == deleteType)
+			return deleteEmployeeMap(fakeDataManagerLastPhoneNumber_, deleteInfomation.getLastPhoneNumber());
+		else if (SelectType::FULL_BIRTHDAY == deleteType)
+			return deleteEmployeeMap(fakeDataManagerFullBirthDay_, fullBrithday);
+		else if (SelectType::YEAR_OF_BIRTHDAY == deleteType)
+			return deleteEmployeeMap(fakeDataManagerYearOfBirthday_, deleteInfomation.getBirthday().getYear());
+		else if (SelectType::MONTH_OF_BIRTHDAY == deleteType)
+			return deleteEmployeeMap(fakeDataManagerMonthOfBirthday_, deleteInfomation.getBirthday().getMonth());
+		else if (SelectType::DAY_OF_BIRTHDAY == deleteType)
+			return deleteEmployeeMap(fakeDataManagerDayOfBirthday_, deleteInfomation.getBirthday().getDay());
+		else if (SelectType::CERTI_LEVEL == deleteType)
+			return deleteEmployeeMap(fakeDataManagerCertiLevel_, deleteInfomation.getCertiLevel());
+	}
+
+	static bool employeeNumberCompare(const unsigned int &a,const unsigned int& b)
+	{
+		unsigned int EmployeeNumberA = a;
+		unsigned int EmployeeNumberB = b;
+
+		if (EmployeeNumberA < 69000000)
+			EmployeeNumberA += 100000000;
+
+		if (EmployeeNumberB < 69000000)
+			EmployeeNumberB += 100000000;
+
+		if (EmployeeNumberA > EmployeeNumberB)
+			return false;
+		else
+			return true;
+	}
+
+	ProcessResult* deleteEmployeeByEmployeeNumber(int empplyeeNumber)
+	{
+		processResult_.numOfRecord = fakeDataManager_.count(empplyeeNumber);
+
+		if (processResult_.numOfRecord)
+			processResult_.printRecord[0] = to_string(fakeDataManager_.at(empplyeeNumber).getEmployeeNumber());
+
+		fakeDataManager_.erase(empplyeeNumber);
+
+		return &processResult_;
+	}
+
+	ProcessResult* deleteEmployeeMap(map <string, vector<unsigned int>> & dataManagerMap, string str)
+	{
+		processResult_.numOfRecord = dataManagerMap[str].size();
+		vector<unsigned int> v = dataManagerMap[str];
+
+		sort(v.begin(), v.end(), employeeNumberCompare);
+
+		int i = 0;
+		for (auto it = v.begin(); it != v.end();++it)
+		{
+			processResult_.printRecord[i++] = to_string(*it);
+			fakeDataManager_.erase(*it);
+		}
+
+		v.clear();
+
+		return &processResult_;
+	}
+
+	ProcessResult* deleteEmployeeMap(map <CareerLevel, vector<unsigned int>>& dataManagerMap, CareerLevel careerLevel)
+	{
+		processResult_.numOfRecord = dataManagerMap[careerLevel].size();
+		vector<unsigned int> v = dataManagerMap[careerLevel];
+		
+		sort(v.begin(), v.end(), employeeNumberCompare);
+		
+		int i = 0;
+		for (auto it = v.begin(); i < 5 && it != v.end(); ++it)
+		{
+			processResult_.printRecord[i++] = to_string(*it);
+		}
+
+		for (auto it = v.begin(); it != v.end(); ++it)
+		{
+			fakeDataManager_.erase(*it);
+		}
+
+		v.clear();
+
+		return &processResult_;
+	}
+
+	ProcessResult* deleteEmployeeMap(map <unsigned int, vector<unsigned int>>& dataManagerMap, unsigned int searchInt)
+	{
+		processResult_.numOfRecord = dataManagerMap[searchInt].size();
+		vector<unsigned int> v = dataManagerMap[searchInt];
+
+		sort(v.begin(), v.end(), employeeNumberCompare);
+
+		int i = 0;
+		for (auto it = v.begin(); i < 5 && it != v.end(); ++it)
+		{
+			processResult_.printRecord[i++] = to_string(*it);
+		}
+
+		for (auto it = v.begin(); it != v.end(); ++it)
+		{
+			fakeDataManager_.erase(*it);
+		}
+
+		v.clear();
+
+		return &processResult_;
+	}
+
+
+	ProcessResult* deleteEmployeeMap(map <CertiLevel, vector<unsigned int>>& dataManagerMap, CertiLevel certiLevel)
+	{
+		processResult_.numOfRecord = dataManagerMap[certiLevel].size();
+		vector<unsigned int> v = dataManagerMap[certiLevel];
+
+		sort(v.begin(), v.end(), employeeNumberCompare);
+
+		int i = 0;
+		for (auto it = v.begin(); i < 5 && it != v.end(); ++it)
+		{
+			processResult_.printRecord[i++] = to_string(*it);
+		}
+
+		for (auto it = v.begin(); it != v.end(); ++it)
+		{
+			fakeDataManager_.erase(*it);
+		}
+
+		v.clear();
+
+		return &processResult_;
+	}
+
+	ProcessResult* getProcessResult() {
+		return &processResult_;
+	}
+
+	size_t getNumOffakeDataManager()
+	{
+		return fakeDataManager_.size();
+	}
+
+protected:
+	map <unsigned int, EmployeeInfomation> fakeDataManager_;
+	map <string, vector<unsigned int>> fakeDataManagerFullName_;
+	map <string, vector<unsigned int>> fakeDataManagerFristName_;
+	map <string, vector<unsigned int>> fakeDataManagerLastName_;
+	map <CareerLevel, vector<unsigned int>> fakeDataManagerCareerLevel_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerFullPhoneNumber_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerMidPhoneNumber_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerLastPhoneNumber_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerFullBirthDay_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerYearOfBirthday_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerMonthOfBirthday_;
+	map <unsigned int, vector<unsigned int>> fakeDataManagerDayOfBirthday_;
+	map <CertiLevel, vector<unsigned int>> fakeDataManagerCertiLevel_;
+
+private:
+	ProcessResult processResult_;
+};
+
+
+class MockDataManager : public IDataManager {
+public:
+	MOCK_METHOD(void, addEmployee, (EmployeeInfomation& employeeInfomation), (override));
+	MOCK_METHOD(ProcessResult*, deleteEmployee, (const bool isDetailPrint, const SelectType deleteType, EmployeeInfomation& deleteInfomation), (override));
+
+	void DelegateToFake() {
+		ON_CALL(*this, addEmployee).WillByDefault([this](EmployeeInfomation& employeeInfomation) {return fake_.addEmployee(employeeInfomation); });
+		ON_CALL(*this, deleteEmployee).WillByDefault([this](const bool isDetailPrint, const SelectType deleteType, EmployeeInfomation& deleteInfomation)-> ProcessResult* {return fake_.deleteEmployee(isDetailPrint, deleteType, deleteInfomation); });
+	}
+
+	size_t getNumOfFakeDataManager()
+	{
+		return fake_.getNumOffakeDataManager();
+	}
+
+	ProcessResult*  getProcessResult()
+	{
+		return fake_.getProcessResult();
+	}
+
+private:
+	FakeDataManager fake_;  // Keeps an instance of the fake in the mock.
+
+};
--- UnitTest/Processor_unittest.h
+++ UnitTest/Processor_unittest.h
@@ -1,10 +1,49 @@
 #pragma once
 
 #include "pch.h"
-#include "MockAddCommandProcessor.h"
+#include "MockDataManager.h"
 #include "../AceAgent/Processor.cpp"
 
-class FixtureAddCommandProcessor : public ::testing::Test {
+class FixtureCommandProcessor : public ::testing::Test {
+protected:
+	void SetUp() override {
+		mockProcessor_.DelegateToFake();
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(15123099, "VXIHXOTH", "JHOP", CareerLevel::CL3, 3112, 2609, 1977, 12, 11, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(17112609, "FB", "NTAWR", CareerLevel::CL4, 5645, 6122, 1986, 12, 3, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18115040, "TTETHU", "HBO", CareerLevel::CL3, 4581, 2050, 2008, 7, 18, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(88114052, "NQ", "LVARW", CareerLevel::CL4, 4528, 3059, 1991, 10, 21, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(19129568, "SRERLALH", "HMEF", CareerLevel::CL2, 3091, 9521, 1964, 9, 10, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(17111236, "VSID", "TVO", CareerLevel::CL1, 3669, 1077, 2012, 7, 18, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18117906, "TWU", "QSOLT", CareerLevel::CL4, 6672, 7186, 2003, 4, 13, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(8123556, "WN", "XV", CareerLevel::CL1, 7986, 5047, 2010, 6, 14, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(2117175, "SBILHUT", "LDEXRI", CareerLevel::CL4, 2814, 1699, 1995, 7, 4, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(3113260, "HH", "LTUPF", CareerLevel::CL2, 5798, 5383, 1979, 10, 18, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(14130827, "RPO", "JK", CareerLevel::CL4, 3231, 1698, 2009, 2, 1, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(1122329, "DN", "WD", CareerLevel::CL4, 7174, 5680, 2007, 11, 17, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(8108827, "RTAH", "VNUP", CareerLevel::CL4, 9031, 2726, 1978, 4, 17, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(85125741, "FBAH", "RTIJ", CareerLevel::CL1, 8900, 1478, 1978, 2, 28, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(8117441, "BMU", "MPOSXU", CareerLevel::CL3, 2703, 3153, 2001, 2, 15, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(10127115, "KBU", "MHU", CareerLevel::CL3, 3284, 4054, 1966, 8, 14, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(12117017, "LFIS", "JJIVL", CareerLevel::CL1, 7914, 4067, 2012, 8, 12, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(11125777, "TKOQKIS", "HC", CareerLevel::CL1, 6734, 2289, 1999, 10, 1, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(11109136, "QKAHCEX", "LTODDO", CareerLevel::CL4, 2627, 8566, 1964, 1, 30, CertiLevel::PRO)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(5101762, "VCUHLE", "HMU", CareerLevel::CL4, 3988, 9289, 2003, 8, 19, CertiLevel::PRO)));
+		
+		//Full name 중복 데이터
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18115041, "TTETHU", "HBO", CareerLevel::CL3, 4581, 2050, 2008, 7, 18, CertiLevel::ADV)));
+
+		//First name 중복 데이터
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18115042, "TTETHU", "KIM", CareerLevel::CL3, 4582, 2050, 2008, 7, 18, CertiLevel::ADV)));
+
+		//Last name 중복 데이터
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18115043, "ABC", "HBO", CareerLevel::CL3, 4581, 2051, 2008, 7, 18, CertiLevel::ADV)));
+		mockProcessor_.addEmployee(*(new EmployeeInfomation(18115044, "DEF", "HBO", CareerLevel::CL3, 4582, 2051, 2008, 7, 18, CertiLevel::ADV)));
+	
+	}
+
+	void TearDown() override {
+	}
+
 public:
-	testing::NiceMock<MockAddCommandProcessor> mockProcessor_;
+	testing::NiceMock<MockDataManager> mockProcessor_;
 };
